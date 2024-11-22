a9db72bf73dd299b6870b3de0136dcba8a8fe604
akshayitzme <akshayitzme.official@gmail.com>
2024-11-22 16:05:58 +0530

    feat: fetchFromErpnext modal

--- models/index.ts
+++ models/index.ts
@@ -52,6 +52,7 @@ import { POSOpeningShift } from './inventory/Point of Sale/POSOpeningShift';
 import { POSClosingShift } from './inventory/Point of Sale/POSClosingShift';
 import { ERPNextSyncSettings } from './baseModels/ERPNextSyncSettings/ERPNextSyncSettings';
 import { ERPNextSyncQueue } from './baseModels/ERPNextSyncQueue/ERPNextSyncQueue';
+import { FetchFromERPNextQueue } from './baseModels/FetchFromERPNextQueue/FetchFromERPNextQueue';
 
 export const models = {
   Account,
@@ -110,6 +111,7 @@ export const models = {
   // ERPNext Sync
   ERPNextSyncSettings,
   ERPNextSyncQueue,
+  FetchFromERPNextQueue,
 } as ModelMap;
 
 export async function getRegionalModels(
